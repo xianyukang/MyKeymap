0befce775836dd42ea8084ba023111f1a50582eb
xianyukang <850111596@qq.com>
2021-04-08 09:00:34 +0800

    完成配置文件读写

--- config-back/api.py
+++ config-back/api.py
@@ -1,7 +1,28 @@
-
+import os
+import json
+from flask import jsonify
+from flask import request
 from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
+
+@app.route('/', methods=['GET'])
+def index_page():
+    return '这不是主页'
+
+
+@app.route('/config', methods=['GET'])
+def get_config():
+    with open('config.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        return jsonify(data)
+
+@app.route('/config', methods=['PUT'])
+def save_config():
+    data = request.get_json()
+    with open('config.json', 'r+', encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
+        f.truncate()
+    return 'ok'
