7ebda1b79bca2efbac441cd4e9243e797c8201d4
xianyukang <850111596@qq.com>
2021-09-12 16:33:43 +0800

    数字雨特效在 python 里不好搞,  有多线程问题,  去掉这个特效吧

--- config-back/api.py
+++ config-back/api.py
@@ -36,14 +36,14 @@ def save_config():
 
 if __name__ == '__main__':
     import logging
-    os.system('chcp 65001')
-    os.system('cls')
     log = logging.getLogger('werkzeug')
     log.setLevel(logging.ERROR)
+    os.system('chcp 65001')
+    os.system('cls')
     print()
-    print(' ------------------------------------------------------------------')
-    print(' 1. 打开浏览器访问 http://localhost:12333 修改 MyKeyamp 的配置')
-    print(' 2. 修改并保存配置后按 alt+\' 可重启 MyKeymap (这里的\'是单引号键) ')
-    print(' 3. 修改完 MyKeymap 的配置后即可关闭本窗口')
-    print(' ------------------------------------------------------------------')
-    app.run(port=12333, debug=True)
+    print('   ------------------------------------------------------------------')
+    print('   1. 打开浏览器访问 http://localhost:12333 修改 MyKeyamp 的配置')
+    print('   2. 保存配置后需要按 alt+\' 重启 MyKeymap (这里的\'是单引号键) ')
+    print('   3. 修改完 MyKeymap 的配置后即可关闭本窗口')
+    print('   ------------------------------------------------------------------')
+    app.run(port=12333, debug=False)
