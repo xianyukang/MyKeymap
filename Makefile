a94d9a28f6249102ddbcb0a764a9cda151ccff36
xianyukang <850111596@qq.com>
2023-08-21 09:01:00 +0800

    安装 vueuse; 发现 WSL2 热重载有坑

--- Makefile
+++ Makefile
@@ -7,7 +7,10 @@ ahk: go
 server: go
 	@cd config-server; ./settings.exe debug
 
+# https://github.com/facebook/create-react-app/issues/10253#issuecomment-747970009
+# 坑啊: WSL2 中的 Chokidar 无法监测 Windows 上的文件修改, 导致 hot reload 不起作用
+# 需要把项目移到 WSL2 里面, 或者使用 Polling 模式
 client:
-	@cd config-ui; npm run dev
+	@cd config-ui; CHOKIDAR_USEPOLLING=true npm run dev
 
 .PHONY: server client ahk go
--- config-ui/src/views/Home.vue
+++ config-ui/src/views/Home.vue
@@ -1,7 +1,12 @@
-<template>
-  <HelloWorld />
-</template>
-
 <script lang="ts" setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+import { useFetch } from '@vueuse/core'
+const url = 'http://localhost:12333/config'
+const { isFetching, data, error } = useFetch(url)
+
 </script>
+
+<template>
+  <div v-if="error" style="color: red;">{{ error }}</div>
+  <div v-else-if="isFetching">loading...</div>
+  <div v-else>{{ data }}</div>
+</template>
