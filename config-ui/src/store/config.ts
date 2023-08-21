a2ec2d214c7fa6bf8fe86827055952c2efdbd7aa
xianyukang <850111596@qq.com>
2023-08-21 21:47:23 +0800

    返回当前选中的 action

--- config-ui/src/store/config.ts
+++ config-ui/src/store/config.ts
@@ -1,18 +1,7 @@
 import { defineStore } from "pinia"
 import { useFetch } from '@vueuse/core'
-import { computed } from "vue"
+import { computed, ref } from "vue"
 import { useRoute } from "vue-router"
-
-interface Config {
-  keymaps: Keymap[]
-}
-
-interface Keymap {
-  id: number
-  name: string
-  enable: boolean
-}
-
 
 export const useConfigStore = defineStore('config', () => {
   const url = 'http://localhost:12333/config'
@@ -27,5 +16,53 @@ export const useConfigStore = defineStore('config', () => {
     return data.value?.keymaps.find(x => x.id + '' === route.params.id)
   })
 
-  return { isFetching, config: data, error, enabledKeymaps, keymap }
+  // 根据选中的 hotkey 和 windowGroupID, 返回对应的 action
+  const hotkey = ref("")
+  const windowGroupID = ref(0)
+  const action = computed(() => getAction(keymap.value, hotkey.value, windowGroupID.value))
+
+  return { isFetching, config: data, error, enabledKeymaps, keymap, hotkey, windowGroupID, action }
 })
+
+
+function getAction(keymap: Keymap | undefined, hotkey: string, windowGroupID: number): Action | null {
+  if (!keymap || !hotkey) {
+    return null
+  }
+  // keymap 中此热键还不存在, 那么初始化一下
+  let actions = keymap.hotkeys[hotkey]
+  if (!actions) {
+    actions = []
+    keymap.hotkeys[hotkey] = actions
+  }
+
+  // 选择的 windowGroupID 还没有对应的 action, 那么初始化一下
+  let found = actions.find(x => x.windowGroupID === windowGroupID)
+  if (!found) {
+    found = { windowGroupID: windowGroupID }
+    actions.push(found)
+  }
+  return found
+}
+
+
+
+interface Config {
+  keymaps: Keymap[]
+}
+
+interface Keymap {
+  id: number
+  name: string
+  enable: boolean
+  hotkeys: Hotkeys
+}
+
+interface Hotkeys {
+  [key: string]: Action[]
+}
+
+interface Action {
+  windowGroupID: number
+  actionTypeID?: number
+}
--- config-ui/src/views/Keymap.vue
+++ config-ui/src/views/Keymap.vue
@@ -2,13 +2,14 @@
 
 import { useConfigStore } from '@/store/config';
 import { storeToRefs } from 'pinia';
-const { keymap } = storeToRefs(useConfigStore())
+const { keymap, action } = storeToRefs(useConfigStore())
 
 </script>
 
 <template>
   <div>Keymap{{ $route.params.id }}</div>
   <div>{{ keymap }}</div>
+  <div>{{ action }}</div>
 </template>
 
 <style scoped></style>
