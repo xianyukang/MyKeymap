cfeb98ae311f8ab293f1b3f8d8371e0595f58bd6
xianyukang <850111596@qq.com>
2023-08-23 11:45:13 +0800

    router 导入组件时, 组件的定义代码会被执行, 此时访问 store 中的异步数据会空指针异常

--- config-ui/src/views/Keymap.vue
+++ config-ui/src/views/Keymap.vue
@@ -3,20 +3,25 @@ import Action from "@/actions/Action.vue";
 import Key from "@/components/Key.vue";
 import { useConfigStore } from "@/store/config";
 import { storeToRefs } from "pinia";
+import { computed } from "vue";
 const { keymap } = storeToRefs(useConfigStore());
 
-const keyboardRows = [
-  ["*1", "*2", "*3", "*4", "*5", "*6", "*7", "*8", "*9", "*0"],
-  ["*q", "*w", "*e", "*r", "*t", "*y", "*u", "*i", "*o", "*p"],
-  ["*a", "*s", "*d", "*f", "*g", "*h", "*j", "*k", "*l", "*;"],
-  ["*z", "*x", "*c", "*v", "*b", "*n", "*m", "*,", "*.", "*/"],
-  ["*space", "*-", "*=", "*backspace", "*[", "*]", "*\\", "*'", "*enter"],
-];
 
-if (keymap.value.extraHotkeys) {
-  const lastRow = keyboardRows[keyboardRows.length - 1];
-  lastRow.push(...keymap.value.extraHotkeys);
-}
+const keyboardRows = computed(() => {
+  const rows = [
+    ["*1", "*2", "*3", "*4", "*5", "*6", "*7", "*8", "*9", "*0"],
+    ["*q", "*w", "*e", "*r", "*t", "*y", "*u", "*i", "*o", "*p"],
+    ["*a", "*s", "*d", "*f", "*g", "*h", "*j", "*k", "*l", "*;"],
+    ["*z", "*x", "*c", "*v", "*b", "*n", "*m", "*,", "*.", "*/"],
+    ["*space", "*-", "*=", "*backspace", "*[", "*]", "*\\", "*'", "*enter"],
+  ];
+  if (keymap.value!.extraHotkeys) {
+    const lastRow = rows[rows.length - 1];
+    lastRow.push(...keymap.value!.extraHotkeys);
+  }
+  return rows
+})
+
 
 
 </script>
