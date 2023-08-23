59a72088deb6cf84b443b3cc2e49bbec5a230d12
xianyukang <850111596@qq.com>
2023-08-23 11:39:43 +0800

    不允许修改 computed 返回的对象, 因为每一次重新计算 computed 都会返回新的快照

--- config-ui/src/store/config.ts
+++ config-ui/src/store/config.ts
@@ -1,35 +1,47 @@
 import { defineStore } from "pinia"
 import { useFetch } from '@vueuse/core'
-import { computed, ref } from "vue"
+import { computed, ref, watch } from "vue"
 import { useRoute } from "vue-router"
+
+const emptyAction: Action = {
+  windowGroupID: 0,
+  actionTypeID: 0,
+}
 
 export const useConfigStore = defineStore('config', () => {
   const url = 'http://localhost:12333/config'
-  const { isFetching, data, error } = useFetch(url).json<Config>()
+  const { isFetching, data: config, error } = useFetch(url).json<Config>()
 
-  const enabledKeymaps = computed(() => {
-    return data.value!.keymaps.filter(x => x.enable)
-  })
-
+  // 根据 url 返回对应的 keymap
   const route = useRoute()
-  const keymap = computed(() => {
-    return data.value!.keymaps.find(x => x.id + '' === route.params.id)!
-  })
+  const keymap = ref<Keymap>()
+  watch(
+    () => config.value?.keymaps.find(x => x.id + '' === route.params.id),
+    (newValue) => keymap.value = newValue
+  )
 
   // 根据选中的 hotkey 和 windowGroupID, 返回对应的 action
   const hotkey = ref("")
   const windowGroupID = ref(0)
-  const action = computed(() => _getAction(keymap.value, hotkey.value, windowGroupID.value))
+  const action = ref(emptyAction)
+  watch(
+    () => _getAction(keymap.value, hotkey.value, windowGroupID.value),
+    (newValue) => action.value = newValue
+  )
 
-  function getAction(hotkey: string) {
-    return _getAction(keymap.value, hotkey, windowGroupID.value)
+  return {
+    isFetching, config, error, keymap, hotkey, windowGroupID, action,
+    getAction: (hotkey: string) => _getAction(keymap.value, hotkey, windowGroupID.value),
+    enabledKeymaps: computed(() => config.value?.keymaps.filter(x => x.enable)),
   }
-
-  return { isFetching, config: data, error, enabledKeymaps, keymap, hotkey, windowGroupID, action, getAction }
 })
 
 
-function _getAction(keymap: Keymap, hotkey: string, windowGroupID: number): Action {
+function _getAction(keymap: Keymap | undefined, hotkey: string, windowGroupID: number): Action {
+  if (!keymap || !hotkey) {
+    return emptyAction
+  }
+
   // keymap 中此热键还不存在, 那么初始化一下
   let actions = keymap.hotkeys[hotkey]
   if (!actions) {
@@ -40,7 +52,7 @@ function _getAction(keymap: Keymap, hotkey: string, windowGroupID: number): Acti
   // 选择的 windowGroupID 还没有对应的 action, 那么初始化一下
   let found = actions.find(x => x.windowGroupID === windowGroupID)
   if (!found) {
-    found = { windowGroupID: windowGroupID, actionTypeID: 0 }
+    found = emptyAction
     actions.push(found)
   }
   return found
@@ -68,7 +80,7 @@ interface Hotkeys {
 
 export interface Action {
   windowGroupID: number
-  actionTypeID?: number
+  actionTypeID: number
 }
 
 interface Options {
