b9d6aefa38f123287f86e6697e674fa8a8a84d74
xianyukang <850111596@qq.com>
2023-08-27 22:40:19 +0800

    完成 action 类别: 重映射按键

--- config-ui/src/actions/RemapKey.vue
+++ config-ui/src/actions/RemapKey.vue
@@ -1,14 +1,31 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { useConfigStore } from '@/store/config';
+import { storeToRefs } from 'pinia';
+const { action } = storeToRefs(useConfigStore())
+const label = "重映射为"
+const items = [
+  'up', 'down', 'left', 'right', 'home', 'end', 'backspace', 'delete',
+  'space', 'tab', 'enter', 'esc', 'insert', 'capslock', 'appskey', 'pgup', 'pgdn',
+  'LWin', 'RWin', 'LCtrl', 'RCtrl', 'LAlt', 'RAlt', 'LShift', 'RShift', 'PrintScreen',
+  'volume_mute', 'volume_up', 'volume_down', 'media_next', 'media_prev', 'media_stop', 'media_play_pause',
+  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+  'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12',
+]
 
 </script>
 
 <template>
-  <div>
-    remap key
-  </div>
+  <!-- :menu-props="{ maxHeight: '390px' }" 可以调整下拉框高度 -->
+  <v-autocomplete class="input"
+                  :label="label"
+                  :items="items"
+                  :hide-no-data="true"
+                  v-model="action.remapToKey"
+                  variant="underlined"></v-autocomplete>
 </template>
 
 <style scoped>
-
+.input :deep(i) {
+  visibility: hidden;
+}
 </style>
