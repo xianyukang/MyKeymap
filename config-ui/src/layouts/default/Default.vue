34d0b80f00b23c73987151f5bbe1ebb45567df0c
CCCC-L <nageniaiwome@163.com>
2023-08-27 14:25:03 +0800

    监听键盘按下ctrl+s键

--- config-ui/src/layouts/default/Default.vue
+++ config-ui/src/layouts/default/Default.vue
@@ -9,5 +9,16 @@
 import { useConfigStore } from '@/store/config';
 import NavigationDrawer from './NavigationDrawer.vue';
 import DefaultView from './View.vue'
+import { onMounted } from "vue";
 const store = useConfigStore()
+
+onMounted(() => {
+  window.addEventListener("keydown", evt => {
+    if (evt.ctrlKey && evt.key.toLowerCase() === 's') {
+      evt.preventDefault()
+      store.saveConfig()
+    }
+  })
+})
+
 </script>
