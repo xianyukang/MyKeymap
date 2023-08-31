0cec7501b9b7c3245f2de3f6c5fe4b09bc1bac43
CCCC-L <nageniaiwome@163.com>
2023-08-31 12:43:07 +0800

    简化缩写Abbr中rn命令

--- config-ui/src/views/Abbr.vue
+++ config-ui/src/views/Abbr.vue
@@ -23,9 +23,8 @@ const runCmd = () => {
     removeHotkey(cmdStr.substring(4))
   } else if (cmdStr.startsWith("rn ")) {
     // 重命名
-    let split = cmdStr.substring(3).split(" ")
-    changeHotkey(split[0], split[1])
-    cmdStr = split[1]
+    cmdStr = cmdStr.substring(3)
+    changeHotkey(useConfigStore().hotkey, cmdStr)
   }
 
   useConfigStore().hotkey = cmdStr.startsWith("del ") ? "" : cmdStr
@@ -49,8 +48,7 @@ const formatSpace = (hotkey: string) => {
 
       <v-text-field v-model="cmd" @keydown.enter="runCmd()"
                     class="ml-1 mt-5" variant="underlined"
-                    label="输入ab按回车添加/切换到ab, del ab删除ab, rn cd重命名当前为cd"
-                    hint="虽然支持空格但是不建议使用空格，因为带有空格重命名的时候不好判断旧新名称">
+                    label="输入ab按回车添加/切换到ab, del ab删除ab, rn cd重命名当前为cd">
       </v-text-field>
     </v-card-text>
   </v-card>
