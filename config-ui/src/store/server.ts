6147adf9693a52e15be3c39641e196de30b8b725
xianyukang <850111596@qq.com>
2023-11-10 17:36:00 +0800

    检查超时

--- config-ui/src/store/server.ts
+++ config-ui/src/store/server.ts
@@ -2,6 +2,15 @@ import { createFetch } from "@vueuse/core"
 
 export const useMyFetch = createFetch({
   baseUrl: import.meta.env.MODE == 'development' ? 'http://localhost:12333' : '',
+  options: {
+    timeout: 1500,
+    onFetchError(ctx) {
+      if (ctx.error.code == 20) {
+        alert("请求超时，可能设置程序被关了")
+      }
+      return ctx
+    }
+  }
 })
 
 
